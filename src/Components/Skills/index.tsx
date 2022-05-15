56d2c8fcf35a14a6cfab4cec822c412ac9cfc773
Gabriel Vinicius <gabrielviniciusdesouzaoliveira@gmail.com>
2022-05-15 21:43:18 +0100

    commit

--- src/Components/Skills/index.tsx
+++ src/Components/Skills/index.tsx
@@ -1,4 +1,4 @@
-import { Chart } from "../pieChart";
+
 import { Component } from "./styled";
 
 
@@ -6,11 +6,7 @@ import { Component } from "./styled";
 export function Skills (){
     return(
         <Component>
-          <Chart 
-          class ="pie animation"
-          name="HTML"
-          porcent={55}
-          />
+          
         </Component>
     )
 }
